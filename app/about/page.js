97003eca1ec73eedbf4295021b12304f1628b3bc
Marais Devis <maraisdcdevis@gmail.com>
2024-06-03 18:05:49 +0200

    Create basic skeleton of site with nav bar

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'About',
+  description: 'Ecommerce project to simulate a store selling cookies',
+};
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>this will be about the store</p>
+    </div>
+  );
+}
---
+++ app/products/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Products',
+  description: 'Ecommerce project to simulate a store selling cookies',
+};
+
+export default function ProductsPage() {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <p>this page will list all the available products</p>
+    </div>
+  );
+}
